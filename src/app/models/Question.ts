57e60ceb7821b633b71b1596d7d39dd488625861
Luis Pintado <luchopintado@gmail.com>
2017-08-11 13:34:53 -0500

    Answer toggle and DataService

---
+++ src/app/models/Question.ts
@@ -0,0 +1,5 @@
+export class Question {
+    text: string;
+    answer: string;
+    hide: boolean;
+}
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Question } from '../models/Question';
+
+@Injectable()
+export class DataService {
+
+  questions: Question[];
+
+  constructor() {
+    this.questions = [
+      {
+        text: 'Do you know Angular 4?',
+        answer: 'Oh yeah! :D',
+        hide: true
+      },
+      {
+        text: 'What is your favorite framework?',
+        answer: 'Im not sure =S',
+        hide: true
+      },
+      {
+        text: 'Do you know ES6?',
+        answer: 'Oh yeah! Is pretty awesome!!',
+        hide: true
+      }
+    ];
+  }
+
+  getQuestions () {
+    return this.questions;
+  }
+
+}
